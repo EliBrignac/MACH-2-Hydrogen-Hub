b54c852128ae5124be99b26f58703bd36857a349
Eli Brignac <ebrignac@udel.edu>
2024-05-14 13:48:52 -0400

    feat: plotted cluster vis, and samples from each plotted cluster

--- app.py
+++ app.py
@@ -120,3 +120,76 @@ Validating model for:
 # button_html = f'<a target="_blank" href="{validation_url}" style="text-decoration: none;"><button style="color: white; background-color: blue; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer;">Validate Model</button></a>'
 # st.markdown(button_html, unsafe_allow_html=True)
 
+
+
+# Most relevant requirements
+st.subheader("Most Relevant Requirements")
+
+
+st.markdown("""
+- Selecting, designing, and maintaining electrolyzers, vessels, compressors, piping systems, and fittings for reliable hydrogen operation.
+- Focus on appropriate material selection, design, and maintenance to withstand high-pressure hydrogen environments, particularly in fuel tanks and related infrastructure.
+- knowledge of materials, coatings, and inhibitors aimed at protecting against hydrogen corrosion and embrittlement.
+- Ensuring the durability and safety of equipment and systems in handling hydrogen under various pressures and temperatures.
+- Knowledge of hydrogen related regulations, standards and codes and keeping up-to-date with the latest changes.
+- Understanding of combined-cycle power generation using hydrogen blending, electrochemical reactions, PEM electrolyzers, hydrogen compression processes
+
+""")
+
+
+import streamlit.components.v1 as components
+from jinja2 import Template
+def render_file():
+    # Your dynamic data
+    app_title = "My Streamlit App"
+    items = ["Item 1", "Item 2", "Item 3"]
+
+    # Load the Jinja2 template
+    path_to_html = r"cluster_viz_plot.html" 
+
+    with open(path_to_html, "r",  encoding='utf-8') as template_file:
+        template_content = template_file.read()
+        jinja_template = Template(template_content)
+
+    # Render the template with dynamic data
+    rendered_html = jinja_template.render(title=app_title, items=items)
+
+    # Display the HTML in Streamlit app
+    components.html(rendered_html, height=800, width=1200, scrolling=True)
+render_file()
+
+option = st.selectbox(
+    'Example Sentences From Clusters:',
+    ('Cluster 0', 
+     'Cluster 1',
+     'Cluster 2',
+     'Cluster 3',
+     'Cluster 4',
+     'Cluster 5',
+     'Cluster 6',
+     'Cluster 7',
+     'Cluster 8',
+     'Cluster 9',)
+)
+
+cluster_sentence_map = {
+    'Cluster 0': ['Hydrogen properties, behaviour and potential hazards created (this is the biggest cluster and the most basic)'],
+    'Cluster 1': ['Safety when working with or around hydrogen (this is second biggest cluster and also very basic)'],
+    'Cluster 2': ['Appropriate selection, design and maintenance of hydrogen fuel tanks, piping systems and fitting, valves and seals to withstand hydrogen pressure (high/low) and temperatures (hot/cold)',
+                  'Assess integrity of vessels, tanks, piping systems and fitting, valves and seals to withstand hydrogen pressure and temperatures',
+                  'Design and selection of  pressure vessels, piping  systems and fitting, valves and  seals, coatings and insulation  to withstand hydrogen pressure and temperatures'],
+    'Cluster 3': ['Knowledge of hydrogen related regulations, standards and codes'],
+    'Cluster 4': ['Keep up-to-date with changes to hydrogen technology, regulations,standards and codes'],
+    'Cluster 5': ['"specific skills/knowledge "'],
+    'Cluster 6': ['Understanding of combined-cycle power generation using hydrogen blending'],
+    'Cluster 7': ['Nothing that impacts this occupation’s ability to apply expertise to manufacturing for hydrogen supply chains'],
+    'Cluster 8': ['Understanding of electrochemical reactions, processes and hydrogen production using PEM electrolyzers',
+                  'Understanding of hydrogen production process using PEM electrolyzers'],
+    'Cluster 9': ['Knowledge and selection of type of materials, coatings and inhibitors to use to protect from hydrogen corrosion',
+                  'Materials, coatings and inhibitorsto use and correct application toprotect from hydrogen corrosion'],
+}
+
+
+display_sentences = cluster_sentence_map[option]
+for sentence in display_sentences:
+    st.write(f'- {sentence}')
